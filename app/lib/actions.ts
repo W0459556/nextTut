f8c39c265a4680d54ef004c3eae104cea6bfb383
Breanna Young <w0459556@campus.nscc.ca>
2025-03-07 12:57:01 -0400

    chore: complete chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 
 const FormSchema = z.object({
@@ -23,6 +25,26 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+//////////////////// auth ////////////////////
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 //////////////////// create ////////////////////
 export type State = {
