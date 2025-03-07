3ee013792e1849e60430a8fbc80e8059cd8444b2
Breanna Young <w0459556@campus.nscc.ca>
2025-03-07 12:51:05 -0400

    fix: remove ESLint 2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from 'next';
- 
+
 const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental'
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
- 
+
 export default nextConfig;
